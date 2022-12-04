12cf8aeb826049962d94d85f31db719c8d45121d
alexander olhovik <just1pman@gmail.com>
2022-12-04 19:14:14 +0300

    fix

--- wp-app/wp-content/themes/kuhni/assets/js/partials/loader.js
+++ wp-app/wp-content/themes/kuhni/assets/js/partials/loader.js
@@ -1,9 +1,13 @@
 function startLoader() {
   const loader = document.querySelector('.loader-container');
-  loader.style.display = "flex";
+  if (loader) {
+    loader.style.display = "flex";
+  }
 }
 
 function stopLoader() {
   const loader = document.querySelector('.loader-container');
-  loader.style.display = "none";
+  if (loader) {
+    loader.style.display = "none";
+  }
 }
