77426572c868fc442de1a4ae5a71a2401c6fd7f2
alexander olhovik <just1pman@gmail.com>
2022-12-12 21:06:56 +0300

    added FAQ module

--- wp-app/wp-content/themes/kuhni/modules/faq/faq.php
+++ wp-app/wp-content/themes/kuhni/modules/faq/faq.php
@@ -8,17 +8,45 @@ Mode: preview
 $textBefore = get_field('text_before');
 $mainContent = get_field('topic');
 $anchor_prefix = 'q-';
+$photo = get_field('photo');
+$mainTitle = get_field('main_title');
 ?>
 
 
 <?php if (!is_admin()) : ?>
     <section class="faq">
+        <?php if(!empty($mainTitle)): ?>
+            <div class="container">
+                <h2 class="faq__main-title"><?= $mainTitle ?></h2>
+            </div>
+        <?php endif; ?>
+        <?php if(!empty($photo)): ?>
+
+            <div class="faq__image-section">
+                <div class="container-s">
+                    <picture>
+                        <img loading="lazy" src="<?= $photo['url'] ?>" alt="">
+                    </picture>
+                </div>
+            </div>
+
+        <?php endif; ?>
+
+
+
         <div class="container">
             <div class="faq__wrapper">
                 <div class="faq__left">
                     <div class="faq__links">
                         <?php foreach ($mainContent as $key => $item) : ?>
-                            <a class="faq__anchor" href="#<?= $anchor_prefix ?><?= $key ?>"><?= $item['title'] ?></a>
+                            <?php if(!empty($item['icon'])) : ?>
+                            <div class="faq__links-wrapper">
+                                <i>
+                                    <img class="faq__icon" width="20" height="20" src="<?= $item['icon']['url'] ?>" alt="">
+                                </i>
+                                <a class="faq__anchor" href="#<?= $anchor_prefix ?><?= $key ?>"><?= $item['title'] ?></a>
+                            </div>
+                            <?php endif; ?>
                         <?php endforeach; ?>
                     </div>
                 </div>
