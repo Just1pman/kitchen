1ca5bd8df7246c016648fc0c7871cdcb00e4f89b
alexander olhovik <just1pman@gmail.com>
2022-12-14 23:24:25 +0300

    added single-kitchen template

--- wp-app/wp-content/themes/kuhni/single-kitchens.php
+++ wp-app/wp-content/themes/kuhni/single-kitchens.php
@@ -24,6 +24,45 @@ get_header();
                 <?= $title ?>
             </h1>
         <?php endif; ?>
+        <div class="kitchen__preview">
+            <div class="kitchen__preview-left">
+1
+            </div>
+            <div class="kitchen__preview-right">
+                <div class="characteristics-block">
+                    <div class="characteristics-block__wrapper">
+                        <h3 class="characteristics-title">Характеристики кухни</h3>
+
+                        <ul class="characteristics-list">
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Стиль</span>
+                                <span class="characteristics-value">Современный</span>
+                            </li>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Планировка</span>
+                                <span class="characteristics-value">2,5 метра</span>
+                            </li>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Материал корпуса</span>
+                                <span class="characteristics-value">Без острова</span>
+                            </li>
+                        </ul>
+                        <span class="characteristics-list__all-btn">Все характеристики</span>
+                    </div>
+                </div>
+
+                <div class="characteristics-block__info-wrapper">
+                    <div class="characteristics-block__button-group">
+                        <button class="characteristics-block__info-order">Оформить заказ</button>
+                        <button class="characteristics-block__info-share">share</button>
+                    </div>
+                    <ul>
+                        <li><img src="" alt="icon"><div><b>Доставка</b><span>В течение 1-2 дней, от 300</span></div></li>
+                        <li><img src="" alt="icon"><div><b>Самовывоз</b><span>По готовности до 18:00, бесплатно</span></div></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
         <div class="kitchen-single__info-module info-module">
             <div class="swiper swiper-single-kitchen">
@@ -56,14 +95,6 @@ get_header();
                 </div>
             </div>
 
-
-
-
-
-
-
-<!--            ////////////////////-->
-
             <div class="info-module__tab-content tab-1 active">
                 <div class="info-module__description-wrapper">
                     <div class="info-module__description-block">
@@ -72,45 +103,45 @@ get_header();
                         <?php endif; ?>
                     </div>
 
-                    <div class="info-module__characteristics-block">
-                        <h3 class="info-module__characteristics-title">Характеристики</h3>
+                    <div class="characteristics-block">
+                        <h3 class="characteristics-title">Характеристики</h3>
 
-                        <ul class="info-module__characteristics-list">
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Стиль</span>
-                                <span class="info-module__characteristics-value">Современный</span>
+                        <ul class="characteristics-list">
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Стиль</span>
+                                <span class="characteristics-value">Современный</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Планировка</span>
-                                <span class="info-module__characteristics-value">2,5 метра</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Планировка</span>
+                                <span class="characteristics-value">2,5 метра</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Материал корпуса</span>
-                                <span class="info-module__characteristics-value">Без острова</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Материал корпуса</span>
+                                <span class="characteristics-value">Без острова</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Материал фасада:</span>
-                                <span class="info-module__characteristics-value">ЛДСП</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Материал фасада:</span>
+                                <span class="characteristics-value">ЛДСП</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Столешница цвет:</span>
-                                <span class="info-module__characteristics-value">Светлая</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Столешница цвет:</span>
+                                <span class="characteristics-value">Светлая</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Покрытие фасада:</span>
-                                <span class="info-module__characteristics-value">Матовое</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Покрытие фасада:</span>
+                                <span class="characteristics-value">Матовое</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Материал корпуса:</span>
-                                <span class="info-module__characteristics-value">ЛДСП</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Материал корпуса:</span>
+                                <span class="characteristics-value">ЛДСП</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Декор корпуса:</span>
-                                <span class="info-module__characteristics-value">«Гейнсборо» и «Дуб Винченца»</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Декор корпуса:</span>
+                                <span class="characteristics-value">«Гейнсборо» и «Дуб Винченца»</span>
                             </li>
-                            <li class="info-module__characteristics-item">
-                                <span class="info-module__characteristics-key">Материал фасадов:</span>
-                                <span class="info-module__characteristics-value">МДФ в плёнке</span>
+                            <li class="characteristics-item">
+                                <span class="characteristics-key">Материал фасадов:</span>
+                                <span class="characteristics-value">МДФ в плёнке</span>
                             </li>
                         </ul>
 
