ee82ddb080818783af4f0543ebb41d5efdc801d4
Denis Shediakov <luckyswd@gmail.com>
2022-12-15 23:20:22 +0300

    plugin

--- wp-app/wp-content/themes/kuhni/assets/js/partials/header.js
+++ wp-app/wp-content/themes/kuhni/assets/js/partials/header.js
@@ -1,6 +1,6 @@
-const burger = document.querySelector('.header-mobile-burger svg');
+const burgerMenu = document.querySelector('.header-mobile-burger svg');
 const html = document.querySelector('html');
 
-burger && burger.addEventListener('click', () => {
+burgerMenu && burgerMenu.addEventListener('click', () => {
   html.classList.toggle('js-menu-active')
 })
