e5eae25f812def3a3e63aef95dfad3277deb6ca4
Denis Shediakov <luckyswd@gmail.com>
2022-12-29 22:45:03 +0300

    add quiz

--- wp-app/wp-content/themes/kuhni/footer.php
+++ wp-app/wp-content/themes/kuhni/footer.php
@@ -36,21 +36,22 @@ $step_5_items = $quiz['step_5'] ?? '';
             <?= $checkout_form ?>
         </div>
     </div>
-    <div class="modal-form form-pick-up" id="pick-up" style="visibility: hidden; display: block">
-        <div class="swiper swiper-pick-up">
-            <h5>Вопрос <span class="current-slide">1</span> из 6</h5>
-            <div class="swiper-wrapper">
-                <div class="swiper-slide" data-number="1">
-                    <h4>Выберите форму вашей кухни</h4>
-                    <?php if (!empty($step_1_items)) : ?>
-                        <div class="step-items">
-                            <?php foreach ($step_1_items['elements'] as $item) : ?>
-                                <div class="item-step">
-                                    <picture>
-                                        <img loading="lazy" src="<?= $item['image']['url'] ?>"
-                                             alt="<?= $item['image']['title'] ?>">
-                                    </picture>
-                                    <div class="checkbox">
+    <?php if (!empty($quiz)) : ?>
+        <div class="modal-form form-pick-up" id="pick-up" style="visibility: hidden; display: block">
+            <div class="swiper swiper-pick-up">
+                <h5>Вопрос <span class="current-slide">1</span> из 6</h5>
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" data-number="1">
+                        <h4>Выберите форму вашей кухни</h4>
+                        <?php if (!empty($step_1_items)) : ?>
+                            <div class="step-items">
+                                <?php foreach ($step_1_items['elements'] as $item) : ?>
+                                    <div class="item-step">
+                                        <picture>
+                                            <img loading="lazy" src="<?= $item['image']['url'] ?>"
+                                                 alt="<?= $item['image']['title'] ?>">
+                                        </picture>
+                                        <div class="checkbox">
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="6" viewBox="0 0 9 6"
                                              fill="none">
@@ -59,43 +60,45 @@ $step_5_items = $quiz['step_5'] ?? '';
                                               fill="white"></path>
                                         </svg>
                                     </span>
-                                        <p><?= $item['text'] ?></p>
+                                            <p><?= $item['text'] ?></p>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="swiper-slide" data-number="2">
-                    <h4>Укажите примерные размеры кухни</h4>
-                    <p class="subheadline">Укажите длину каждой стены на кухне в сантиметрах. Не обращайте внимание на
-                        выступы, ниши, двери и окна</p>
-                    <div class="step-2">
-                        <input class="height" type="number" min="0" placeholder="2.5">
-                        <input class="length" type="number" min="0" placeholder="2.5">
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
-                    <div class="step-2-image">
-                        <div class="step-2-top"><span>2.5</span><span>м</span></div>
-                        <div class="step-2-bottom">
-                            <div><span>2.5</span><span>м</span></div>
-                            <picture>
-                                <img src="<?= home_url() . '/wp-content/uploads/2022/12/угловая.png' ?>" alt="">
-                            </picture>
+                    <div class="swiper-slide" data-number="2">
+                        <h4>Укажите примерные размеры кухни</h4>
+                        <p class="subheadline">Укажите длину каждой стены на кухне в сантиметрах. Не обращайте внимание
+                            на
+                            выступы, ниши, двери и окна</p>
+                        <div class="step-2">
+                            <input class="height" type="number" min="0" placeholder="2.5">
+                            <input class="length" type="number" min="0" placeholder="2.5">
+                        </div>
+                        <div class="step-2-image">
+                            <div class="step-2-top"><span>2.5</span><span>м</span></div>
+                            <div class="step-2-bottom">
+                                <div><span>2.5</span><span>м</span></div>
+                                <picture>
+                                    <img src="<?= home_url() . '/wp-content/uploads/2022/12/угловая.png' ?>" alt="">
+                                </picture>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="swiper-slide" data-number="3">
-                    <h4>Выберите материал фасада</h4>
-                    <p class="subheadline">Цвет или эмитацию фасада можо выбрать позже. Это не вияется на стоимость</p>
-                    <?php if (!empty($step_3_items)) : ?>
-                        <div class="step-items">
-                            <?php foreach ($step_3_items['elements'] as $item) : ?>
-                                <div class="item-step">
-                                    <picture>
-                                        <img loading="lazy" src="<?= $item['image']['url'] ?>"
-                                             alt="<?= $item['image']['title'] ?>">
-                                    </picture>
-                                    <div class="checkbox">
+                    <div class="swiper-slide" data-number="3">
+                        <h4>Выберите материал фасада</h4>
+                        <p class="subheadline">Цвет или эмитацию фасада можо выбрать позже. Это не вияется на
+                            стоимость</p>
+                        <?php if (!empty($step_3_items)) : ?>
+                            <div class="step-items">
+                                <?php foreach ($step_3_items['elements'] as $item) : ?>
+                                    <div class="item-step">
+                                        <picture>
+                                            <img loading="lazy" src="<?= $item['image']['url'] ?>"
+                                                 alt="<?= $item['image']['title'] ?>">
+                                        </picture>
+                                        <div class="checkbox">
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="6" viewBox="0 0 9 6"
                                              fill="none">
@@ -104,27 +107,27 @@ $step_5_items = $quiz['step_5'] ?? '';
                                               fill="white"></path>
                                         </svg>
                                     </span>
-                                        <p><?= $item['text'] ?></p>
+                                            <p><?= $item['text'] ?></p>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="swiper-slide" data-number="4">
-                    <h4>Выберите стиль кухни</h4>
-                    <?php if (!empty($step_4_items)) : ?>
-                        <div class="step-items">
-                            <?php foreach ($step_4_items as $term) : ?>
-                                <?php
-                                $gallery = get_field('gallery', $term->taxonomy . '_' . $term->term_id);
-                                ?>
-                                <div class="item-step">
-                                    <picture>
-                                        <img loading="lazy" src="<?= $gallery[0]['url'] ?? '' ?>"
-                                             alt="<?= $gallery[0]['title'] ?? '' ?>">
-                                    </picture>
-                                    <div class="checkbox">
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="swiper-slide" data-number="4">
+                        <h4>Выберите стиль кухни</h4>
+                        <?php if (!empty($step_4_items)) : ?>
+                            <div class="step-items">
+                                <?php foreach ($step_4_items as $term) : ?>
+                                    <?php
+                                    $gallery = get_field('gallery', $term->taxonomy . '_' . $term->term_id);
+                                    ?>
+                                    <div class="item-step">
+                                        <picture>
+                                            <img loading="lazy" src="<?= $gallery[0]['url'] ?? '' ?>"
+                                                 alt="<?= $gallery[0]['title'] ?? '' ?>">
+                                        </picture>
+                                        <div class="checkbox">
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="6" viewBox="0 0 9 6"
                                              fill="none">
@@ -133,24 +136,24 @@ $step_5_items = $quiz['step_5'] ?? '';
                                               fill="white"></path>
                                         </svg>
                                     </span>
-                                        <p><?= $term->name ?></p>
+                                            <p><?= $term->name ?></p>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="swiper-slide" data-number="5">
-                    <h4>Укажите дополнительные параметры</h4>
-                    <?php if (!empty($step_5_items)) : ?>
-                        <div class="step-items">
-                            <?php foreach ($step_5_items['elements'] as $item) : ?>
-                                <div class="item-step">
-                                    <picture>
-                                        <img loading="lazy" src="<?= $item['image']['url'] ?>"
-                                             alt="<?= $item['image']['title'] ?>">
-                                    </picture>
-                                    <div class="checkbox">
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="swiper-slide" data-number="5">
+                        <h4>Укажите дополнительные параметры</h4>
+                        <?php if (!empty($step_5_items)) : ?>
+                            <div class="step-items">
+                                <?php foreach ($step_5_items['elements'] as $item) : ?>
+                                    <div class="item-step">
+                                        <picture>
+                                            <img loading="lazy" src="<?= $item['image']['url'] ?>"
+                                                 alt="<?= $item['image']['title'] ?>">
+                                        </picture>
+                                        <div class="checkbox">
                                     <span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="6" viewBox="0 0 9 6"
                                              fill="none">
@@ -159,46 +162,50 @@ $step_5_items = $quiz['step_5'] ?? '';
                                               fill="white"></path>
                                         </svg>
                                     </span>
-                                        <p><?= $item['text'] ?></p>
+                                            <p><?= $item['text'] ?></p>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="swiper-slide" data-number="6">
+                        <div class="form-wrapper">
+                            <p class="form-headline">Вы получите расчет стоимости в ближайшее время</p>
+                            <p class="form-subheadline">Наш специалист проверит автоматический расчет цены,
+                                скорректирует
+                                его для более высокой точности и отправит вам в мессенджер</p>
+                            <?= $pick_up ?>
                         </div>
-                    <?php endif; ?>
-                </div>
-                <div class="swiper-slide" data-number="6">
-                    <div class="form-wrapper">
-                        <p class="form-headline">Вы получите расчет стоимости в ближайшее время</p>
-                        <p class="form-subheadline">Наш специалист проверит автоматический расчет цены, скорректирует
-                            его для более высокой точности и отправит вам в мессенджер</p>
-                        <?= $pick_up ?>
                     </div>
                 </div>
-            </div>
-            <div class="navigation">
-                <div class="prev-wrapper">
-                    <div class="prev">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 20 10" fill="none">
-                            <path d="M1.08672 4.07431H16.7981L14.1955 1.48427C13.9051 1.19523 13.9039 0.725515 14.193 0.435107C14.482 0.144662 14.9518 0.143587 15.2422 0.432585L19.1186 4.29035C19.1189 4.29057 19.1191 4.29083 19.1193 4.29105C19.409 4.58009 19.4099 5.05133 19.1194 5.34133C19.1191 5.34155 19.1189 5.34181 19.1187 5.34203L15.2423 9.19979C14.9519 9.48876 14.4821 9.48775 14.1931 9.19727C13.904 8.90686 13.9051 8.43714 14.1955 8.14811L16.7981 5.55807H1.08672C0.676983 5.55807 0.344845 5.22593 0.344845 4.81619C0.344845 4.40645 0.676983 4.07431 1.08672 4.07431Z"
-                                  fill="white"/>
-                        </svg>
+                <div class="navigation">
+                    <div class="prev-wrapper">
+                        <div class="prev">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 20 10"
+                                 fill="none">
+                                <path d="M1.08672 4.07431H16.7981L14.1955 1.48427C13.9051 1.19523 13.9039 0.725515 14.193 0.435107C14.482 0.144662 14.9518 0.143587 15.2422 0.432585L19.1186 4.29035C19.1189 4.29057 19.1191 4.29083 19.1193 4.29105C19.409 4.58009 19.4099 5.05133 19.1194 5.34133C19.1191 5.34155 19.1189 5.34181 19.1187 5.34203L15.2423 9.19979C14.9519 9.48876 14.4821 9.48775 14.1931 9.19727C13.904 8.90686 13.9051 8.43714 14.1955 8.14811L16.7981 5.55807H1.08672C0.676983 5.55807 0.344845 5.22593 0.344845 4.81619C0.344845 4.40645 0.676983 4.07431 1.08672 4.07431Z"
+                                      fill="white"/>
+                            </svg>
+                        </div>
+                        <p>Назад</p>
                     </div>
-                    <p>Назад</p>
-                </div>
 
-                <div class="next-wrapper">
-                    <p>Далее</p>
-                    <div class="next">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 20 10" fill="none">
-                            <path d="M1.08672 4.07431H16.7981L14.1955 1.48427C13.9051 1.19523 13.9039 0.725515 14.193 0.435107C14.482 0.144662 14.9518 0.143587 15.2422 0.432585L19.1186 4.29035C19.1189 4.29057 19.1191 4.29083 19.1193 4.29105C19.409 4.58009 19.4099 5.05133 19.1194 5.34133C19.1191 5.34155 19.1189 5.34181 19.1187 5.34203L15.2423 9.19979C14.9519 9.48876 14.4821 9.48775 14.1931 9.19727C13.904 8.90686 13.9051 8.43714 14.1955 8.14811L16.7981 5.55807H1.08672C0.676983 5.55807 0.344845 5.22593 0.344845 4.81619C0.344845 4.40645 0.676983 4.07431 1.08672 4.07431Z"
-                                  fill="white"/>
-                        </svg>
+                    <div class="next-wrapper">
+                        <p>Далее</p>
+                        <div class="next">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 20 10"
+                                 fill="none">
+                                <path d="M1.08672 4.07431H16.7981L14.1955 1.48427C13.9051 1.19523 13.9039 0.725515 14.193 0.435107C14.482 0.144662 14.9518 0.143587 15.2422 0.432585L19.1186 4.29035C19.1189 4.29057 19.1191 4.29083 19.1193 4.29105C19.409 4.58009 19.4099 5.05133 19.1194 5.34133C19.1191 5.34155 19.1189 5.34181 19.1187 5.34203L15.2423 9.19979C14.9519 9.48876 14.4821 9.48775 14.1931 9.19727C13.904 8.90686 13.9051 8.43714 14.1955 8.14811L16.7981 5.55807H1.08672C0.676983 5.55807 0.344845 5.22593 0.344845 4.81619C0.344845 4.40645 0.676983 4.07431 1.08672 4.07431Z"
+                                      fill="white"/>
+                            </svg>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
     <div class="modal-form form-checkout" id="end-order">
         <div class="form-wrapper">
             <p class="form-headline">Ваша заказ оформлен</p>
