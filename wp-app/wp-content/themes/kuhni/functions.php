b7e6619601ecbfb0cf503d6a6eb8d171460bdc5e
Denis Shediakov <luckyswd@gmail.com>
2023-03-20 17:39:31 +0300

    ym

--- wp-app/wp-content/themes/kuhni/functions.php
+++ wp-app/wp-content/themes/kuhni/functions.php
@@ -72,17 +72,14 @@ function yandexMetrik()
       document.addEventListener('wpcf7mailsent', function (event) {
         if (event.detail.contactFormId == '147') {
           yaCounter92848644.reachGoal('form3');
-          console.log('form3')
         }
 
         if (event.detail.contactFormId == '159') {
           yaCounter92848644.reachGoal('form2');
-          console.log('form2')
         }
 
         if (event.detail.contactFormId == '315') {
           yaCounter92848644.reachGoal('form1');
-          console.log('form1')
         }
       }, false);
     </script>
